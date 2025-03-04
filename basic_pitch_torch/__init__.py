06fbffb0bf2bbe852b636f615c10fd19cedc538a
martingasser <martin.gasser@gmail.com>
2025-03-04 12:52:43 +0100

    added __init__.py

---
+++ basic_pitch_torch/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright 2022 Spotify AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pathlib
+
+def build_icassp_2022_model_path() -> pathlib.Path:
+    return pathlib.Path(__file__).parent / "assets/basic_pitch_pytorch_icassp2022.pth"
+
+ICASSP_MODEL_PATH = build_icassp_2022_model_path()
